f658bb2da4bae189bb861290d4ae090986905f30
Oleg Vetrov <oleg94038@gmail.com>
2022-10-25 23:49:41 +0300

    Added masked view text on Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import MaskedView from '@react-native-masked-view/masked-view';
 import { THEME } from '../theme/theme';
 import { AppTextBold } from './ui/AppTextBold';
 import { Loader } from './Loader';
@@ -8,8 +9,27 @@ export const Header = ({ isLoading }) => {
   return (
     <View style={styles.header}>
       {isLoading
-        ? <Loader/>
-        : <AppTextBold>Todo List</AppTextBold>
+        ? <Loader style={{alignItems: 'center'}}/>
+        : (
+          <MaskedView
+            style={styles.maskContainer}
+            maskElement={
+              <View style={styles.maskView}>
+                <AppTextBold style={styles.text}>Todo List</AppTextBold>
+              </View>
+            }>
+            <View style={{
+              flex: 1,
+              height: '100%',
+              backgroundColor: THEME.COLOR_ORANGE }}
+            />
+            <View style={{
+              flex: 1,
+              height: '100%',
+              backgroundColor: THEME.COLOR_RED }}
+            />
+          </MaskedView>
+        )
       }
     </View>
   );
@@ -18,10 +38,26 @@ export const Header = ({ isLoading }) => {
 const styles = StyleSheet.create({
   header : {
     height: 60,
+    flexDirection: 'row',
     justifyContent: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     backgroundColor: THEME.COLOR_BLUE,
     paddingBottom: 8,
     marginBottom: 16,
+  },
+  text: {
+    fontSize: 22,
+  },
+  maskContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    height: '100%',
+    alignItems: 'flex-end',
+  },
+  maskView: {
+    backgroundColor: 'transparent',
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   }
 });
